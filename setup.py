c14d58a43d7d3e700ed1e9c7f9d785940eb874db
Eric Kern <eakmeister@gmail.com>
2014-02-25 10:30:58 -0500

    Added README and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name = 'bind',
+      version = '1.0',
+      description = 'Argument binding for Python',
+      author = 'Eric Kern',
+      author_email = '',
+      url = '',
+      packages = ['bind']
+ )
+
